69a69f911f5f29aae8c8693705c8eaf272e3f5ff
paul <johnspaulm@gmail.com>
2023-07-24 19:24:41 -0500

    updated model meta

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -10,10 +10,24 @@ class Technician(models.Model):
         unique=True,
     )
 
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
+
+    class Meta:
+        verbose_name_plural = "Technicians"
+        verbose_name = "Technician"
+
 
 class AutomobileVO(models.Model):
     vin = models.CharField(max_length=17)
     sold = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.vin
+
+    class Meta:
+        verbose_name_plural = "Automobile VOs"
+        verbose_name = "Automobile VO"
 
 
 class Appointment(models.Model):
@@ -35,3 +49,10 @@ class Appointment(models.Model):
         on_delete=models.CASCADE,
         related_name="appointment",
     )
+
+    def __str__(self):
+        return f"{self.customer} {self.status}"
+
+    class Meta:
+        verbose_name_plural = "Appointments"
+        verbose_name = "Appointment"
